b9d1379c07060138472a7151fb4e3f4811b6c26a
ngireric123 <ngireric123@gmail.com>
2020-04-14 17:21:03 +0200

    first commit

---
+++ build/precache-manifest.232abd4b8798baca7e6187533ccaf29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3548825e02127416f5a593c06afb634",
+    "url": "/safari/index.html"
+  },
+  {
+    "revision": "7f14aada68219240f7f9",
+    "url": "/safari/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19230d67e67ac96ae885",
+    "url": "/safari/static/js/2.c2553c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/safari/static/js/2.c2553c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f14aada68219240f7f9",
+    "url": "/safari/static/js/main.79d64174.chunk.js"
+  },
+  {
+    "revision": "91d391d579ed1c371335",
+    "url": "/safari/static/js/runtime-main.7c481fe0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/safari/static/media/logo.5d5d9eef.svg"
+  }
+]);
